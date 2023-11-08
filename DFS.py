3dff81bacf233d54b10d81db8001daebb02e4f50
Shayan Shahrabi <shayanshahrab@gmail.com>
2023-11-08 06:15:01 -0800

    add DFS.py file

---
+++ DFS.py
@@ -0,0 +1,45 @@
+from pyMaze import maze, agent, COLOR
+#-----------------------------------------------------------------------------
+def DFS(m):  # takes a maze of any size
+    start = (m.rows, m.cols)  # the start cell is the bottom-down cell
+    explored = [start]
+    frontier = [start]
+    dfs_path = {}
+    while len(frontier) != 0:
+        current_cell = frontier.pop()
+        if current_cell == (1, 1):  # if reached goal cell
+            break
+        for direction in 'ESNW':
+            if m.maze_map[current_cell][direction] == True:
+                if direction == 'E':
+                    child_cell = (current_cell[0], current_cell[1] + 1)
+                elif direction == 'W':
+                    child_cell = (current_cell[0], current_cell[1] - 1)
+                elif direction == 'S':
+                    child_cell = (current_cell[0] + 1, current_cell[1])
+                elif direction == 'N':
+                    child_cell = (current_cell[0] - 1, current_cell[1])
+                if child_cell in explored:
+                    continue
+                explored.append(child_cell)
+                frontier.append(child_cell)
+                dfs_path[child_cell] = current_cell
+    forward_path = {}
+    cell = (1, 1)
+    while cell != start:
+        forward_path[dfs_path[cell]] = cell
+        cell = dfs_path[cell]
+    return forward_path
+#-----------------------------------------------------------------------------
+# main program
+row_num = 20
+col_num = 30
+
+m = maze(row_num, col_num)
+m.CreateMaze()
+
+path = DFS(m)
+a = agent(m, footprints=True)
+m.tracePath({a:path})  # {agent : the path we want the agent to follow}
+
+m.run()  # show the result
